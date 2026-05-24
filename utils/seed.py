43011e27f6ee1a7cccc8ce9799aaee512cfd042b
richardsbrennick-creator <richardsbrennick@gmail.com>
2026-05-24 12:00:38 +0300

    Deploy fix: utils/seed.py

--- utils/seed.py
+++ utils/seed.py
@@ -39,7 +39,7 @@ GM_NAMES = [
 
 def seed_sample_data():
     """Seed DB with sample Nyaka data if empty."""
-    from app import db
+    from extensions import db
     from models import Student, Grandmother
 
     if Student.query.count() > 0:
